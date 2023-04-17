41ecbc9f9fa1cf59d80e5785fac950ab2522c99e
MemeSitar <jurij.sitar@mjau.si>
2023-04-17 17:22:14 +0200

    I am now happy with how this works!
    Added so that gcommit doesn't ask you to add more files when there are no more files to add, prettier colors.

--- gadd.sh
+++ gadd.sh
@@ -8,13 +8,13 @@ then
     exit 1
 else 
     git add "$FILES" 
-    gum style "The following files are added: " --foreground 9
+    gum style "The following files are added: " --foreground 10
     ADDED_OR_MODIFIED=$(git status -s | grep -F -e "A  " -e "M  " | cut -c4-)
     gum style "$ADDED_OR_MODIFIED" --margin="0 2"
 fi
 
 
 # git status -s | grep -F "??" | cut -c4- # shows untracked files
-# git status -s | grep -vF "A  " | cut -c4- # shows edited AND untracked files
+# git status -s | grep -vF -e "A  " -e "M  " | cut -c4- # shows edited AND untracked files
 # git status -s | grep -F "A  " | cut -c4- # shows only added files
 # using ! $FILES helps very much!! use !!!!
--- gcommit.sh
+++ gcommit.sh
@@ -7,17 +7,21 @@ then
     gum style --foreground 9 "Please add files to this commit."
     ./gadd.sh
 else
-    gum style "The following files are added: " --foreground 9 &&
-    gum style "$ADDED_OR_MODIFIED" --margin="1 2" &&
-    gum confirm "Do you want to add more files to this commit?"
-    if [ $? -eq 0 ]; then
-        ./gadd.sh
+    gum style "The following files are added: " --foreground 10 &&
+    gum style "$ADDED_OR_MODIFIED" --margin="0 2" &&
+    EDITED_OR_UNTRACKED=$(git status -s | grep -vF -e "A  " -e "M  " | cut -c4-) &&
+    if [[ $EDITED_OR_UNTRACKED ]]
+    then
+        gum confirm "Do you want to add more files to this commit?"
+        if [ $? -eq 0 ]; then
+            ./gadd.sh
+        fi
     fi
 fi &&
 
 SUMMARY=$(gum input --placeholder "Summary of this change" --char-limit=100) &&
-gum style "$SUMMARY" --margin "1 1" --bold &&
+gum style "SUMMARY: $SUMMARY" --margin "1 1" --bold &&
 DESCRIPTION=$(gum write --prompt="> " --header "Description:" --placeholder="(Ctrl+D to finish)") &&
-gum style "$DESCRIPTION" --margin "1 1" --italic &&
+gum style "DESCRIPTION: $DESCRIPTION" --margin "1 1" --italic &&
 
 gum confirm "Commit?" && git commit -m "$SUMMARY" -m "$DESCRIPTION"
